6746f1dc3ed3b14c34b315d9fa6744abcc64f080
NguyenNgo2108 <nguyenngo2102@gmail.com>
2024-10-17 15:20:38 +0700

    dang ky

---
+++ DangKy/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DangKy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tbEmail = new TextBox();
+            tbPassword = new TextBox();
+            btnDangKy = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // tbEmail
+            // 
+            tbEmail.Location = new Point(323, 131);
+            tbEmail.Name = "tbEmail";
+            tbEmail.Size = new Size(125, 27);
+            tbEmail.TabIndex = 0;
+            tbEmail.TextChanged += tbEmail_TextChanged;
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(323, 201);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(125, 27);
+            tbPassword.TabIndex = 1;
+            tbPassword.TextChanged += tbPassword_TextChanged;
+            // 
+            // btnDangKy
+            // 
+            btnDangKy.Location = new Point(335, 280);
+            btnDangKy.Name = "btnDangKy";
+            btnDangKy.Size = new Size(94, 29);
+            btnDangKy.TabIndex = 2;
+            btnDangKy.Text = "Đăng ký";
+            btnDangKy.UseVisualStyleBackColor = true;
+            btnDangKy.Click += btnDangKy_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(226, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(226, 204);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(354, 47);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Đăng ký";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnDangKy);
+            Controls.Add(tbPassword);
+            Controls.Add(tbEmail);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbEmail;
+        private TextBox tbPassword;
+        private Button btnDangKy;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ DangKy/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace DangKy
+{
+    public partial class Form1 : Form
+    {
+        private ErrorProvider errorProvider;
+        public Form1()
+        {
+            InitializeComponent();
+            errorProvider = new ErrorProvider();
+        }
+
+        private void tbEmail_TextChanged(object sender, EventArgs e)
+        {
+            // Kiểm tra định dạng email
+            string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            if (!Regex.IsMatch(tbEmail.Text, emailPattern))
+            {
+                errorProvider.SetError(tbEmail, "Email không đúng định dạng.");
+            }
+            else
+            {
+                errorProvider.SetError(tbEmail, "");
+            }
+        }
+
+        private void tbPassword_TextChanged(object sender, EventArgs e)
+        {
+            tbPassword.PasswordChar = '*';
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra mật khẩu
+            string password = tbPassword.Text;
+            if (password.Length < 1 || !Regex.IsMatch(password, @"[!@#$%^&*(),.?""':{}|<>]"))
+            {
+                MessageBox.Show("Mật khẩu phải có ít nhất 1 ký tự đặc biệt.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Nếu tất cả đều đúng, thông báo thành công
+            MessageBox.Show("Đăng ký thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
